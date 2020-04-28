50f3028f109c357d0402757fc46b474d5eb119c1
anderswodenker <me@myworkadaylife.de>
2020-04-28 20:22:52 +0200

    2.2

--- testing.py
+++ testing.py
@@ -1,14 +1,5 @@
 import os
-from shutil import copyfile
 
-
-def update():
-    try:
-        print("copy")
-        copyfile("/home/pi/sams_system/update.py", "/home/pi/update.py")
-        os.system("python3 /home/pi/update.py")
-    except Exception as e:
-        print(e)
-
-
-update()
+pull = os.system("git pull origin master")
+if pull == 0:
+    print("done!")
