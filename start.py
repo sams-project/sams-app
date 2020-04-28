57177575c051662c202d71bb69c9bcb325427a7f
anderswodenker <me@myworkadaylife.de>
2020-04-28 19:55:30 +0200

    2.2

--- start.py
+++ start.py
@@ -1,13 +1,7 @@
 from main.application import Application
 import os
 
-
 app = Application()
 
-if os.path.exists("/home/pi/done"):  # update is done -> delete update file
-    os.system("sudo rm /home/pi/done")
-    os.system("sudo rm /home/pi/update.py")
-
-if not os.path.exists("/home/pi/update"):
-    if app.app_config.local_config.start:
-        app.start()
+if app.app_config.local_config.start:
+    app.start()
