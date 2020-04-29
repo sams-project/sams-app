89eff49e7876f6d6ef7ec95a2b238fd5d370bac0
anderswodenker <me@myworkadaylife.de>
2020-04-29 22:30:15 +0200

    2.0

--- prepare.py
+++ prepare.py
@@ -6,11 +6,11 @@ import os
 
 
 def prepare_all_files():
-    shutil.copyfile("/home/pi/sams_project/application.ini", "/home/pi/application.ini")
-    shutil.copyfile("/home/pi/sams_project/user.ini", "/home/pi/user.ini")
-    shutil.copyfile("/home/pi/sams_project/token.ini", "/home/pi/token.ini")
-    shutil.copyfile("/home/pi/sams_project/online.ini", "/home/pi/online.ini")
-    shutil.copyfile("/home/pi/sams_project/error.ini", "/home/pi/error.ini")
+    shutil.copyfile("/home/pi/sams_system/application.ini", "/home/pi/application.ini")
+    shutil.copyfile("/home/pi/sams_system/user.ini", "/home/pi/user.ini")
+    shutil.copyfile("/home/pi/sams_system/token.ini", "/home/pi/token.ini")
+    shutil.copyfile("/home/pi/sams_system/online.ini", "/home/pi/online.ini")
+    shutil.copyfile("/home/pi/sams_system/error.ini", "/home/pi/error.ini")
     os.mkdir(mapping.app_log)
     print("done")
 
