6479e6ba9b65e48ad55f331065b4df3f666db0a3
anderswodenker <me@myworkadaylife.de>
2020-05-16 20:32:31 +0200

    2.4

--- update.py
+++ update.py
@@ -1,5 +1,4 @@
 import shutil
-from git import Repo
 import configparser
 import os
 import time
@@ -15,6 +14,7 @@ class Updater:
     @staticmethod
     def del_directory():
         try:
+            print("del tree")
             shutil.rmtree('/home/pi/sams_system', ignore_errors=True)
             return True
         except Exception:
@@ -23,21 +23,28 @@ class Updater:
     @staticmethod
     def clone_directory():
         try:
-            Repo.clone_from("https://github.com/anderswodenker/sams-app.git", "sams_system")
-            return True
-        except Exception:
+            clone = os.system("sudo git clone https://github.com/anderswodenker/sams-app.git /home/pi/sams_system")
+            if clone == 0:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(e)
             return False
 
     def update(self):
-        if self.start:
-            if self.del_directory():
-                self.config.set("DEFAULT", "update", "0")
-                with open(self.path, 'w') as configfile:
-                    self.config.write(configfile)
-                if self.clone_directory():
-                    time.sleep(120)
-                    os.system('sudo reboot')
+        try:
+            if self.start:
+                self.del_directory()
+                cloning = self.clone_directory()
+                if cloning:
+                    self.config.set("DEFAULT", "update", "0")
+                    with open(self.path, 'w') as configfile:
+                        self.config.write(configfile)
+        except Exception as e:
+            print(e)
 
 
 updater = Updater()
 updater.update()
+#  v 2.3 !
