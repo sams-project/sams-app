9215314258a8b9af547e1358e944b84ac592d8a0
anderswodenker <me@myworkadaylife.de>
2020-06-10 18:01:35 +0200

    2.4

--- start.py
+++ start.py
@@ -5,4 +5,7 @@ from main.application import Application
 app = Application()
 
 if app.app_config.local_config.start and not app.app_config.local_config.is_update:
-    app.start()
+    try:
+        app.start()
+    except Exception as e:
+        print(e)
